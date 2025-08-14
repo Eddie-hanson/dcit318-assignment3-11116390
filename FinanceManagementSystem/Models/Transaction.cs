48eee85f71ba367711706f79e0ae83b113af0656
Edward Hanson <hans18edward@gmail.com>
2025-08-14 20:45:16 +0000

    Add Account and SavingsAccount classes with transaction handling

---
+++ FinanceManagementSystem/Models/Transaction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    namespace FinanceManagementSystem.Models
+    {
+    // Ensure the project is targeting .NET 5.0 or later to support 'record' types.
+    public record Transaction(int Id, DateTime Date, decimal Amount, string Category);
+    }
