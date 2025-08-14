9212e1084168659cf5b7202511425deb88fc586e
Edward Hanson <hans18edward@gmail.com>
2025-08-14 20:44:21 +0000

    Add Program class with Main method to initialize FinanceApp

---
+++ FinanceManagementSystem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManagementSystem
+{
+        public class Program
+        {
+            public static void Main()
+            {
+                new FinanceApp().Run();
+            }
+        }
+}
